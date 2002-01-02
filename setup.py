24a906d02d66891d440b009b72fff9c34897e1cd
drt <drt@2d70b15c-62de-0310-b45c-a2b26695ab02>
2002-01-02 23:02:30 +0000

    version number incerased
    git-svn-id: http://svn.23.nu/svn/repos/ftpparsemodule/trunk@228 2d70b15c-62de-0310-b45c-a2b26695ab02

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup, Extension
 
 setup(name="ftpparsemodule",
-      version="0.9",
+      version="0.91",
       description="Parsing of FTP server LIST responses",
       author="drt",
       author_email="drt@un.bewaff.net",
