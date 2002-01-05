b20ff6ce55e7c028eb4e41081d9cc584b3bb3a75
drt <drt@2d70b15c-62de-0310-b45c-a2b26695ab02>
2002-01-05 04:50:31 +0000

    Improved distutils support by hartmut goebel
    git-svn-id: http://svn.23.nu/svn/repos/ftpparsemodule/trunk@229 2d70b15c-62de-0310-b45c-a2b26695ab02

--- setup.py
+++ setup.py
@@ -2,9 +2,23 @@
 
 from distutils.core import setup, Extension
 
+long_description = """
+    ftpparse is a library for parsing FTP LIST responses.
+
+    This Python module is completely based on Dan Bernstein's ftpparse
+    library at http://cr.yp.to/ftpparse.html Mr. Bernstein requests you to
+    inform him if you use his library in an commercial application.
+
+    The Python wrapper was hacked by drt@un.bewaff.net - http://c0re.jp/
+
+    You might be able to find more Information and updates at
+    http://c0re.jp/c0de/ftpparse/
+    """
+
 setup(name="ftpparsemodule",
-      version="0.91",
+      version="0.92",
       description="Parsing of FTP server LIST responses",
+      long_description=long_description,
       author="drt",
       author_email="drt@un.bewaff.net",
       url="http://c0re.jp/c0de/ftpparse/",
